ad62536c0edfb0a9b8307a611014f72a279d7f53
Tom Grek <tom.grek@gmail.com>
2020-04-06 20:28:50 -0700

    Make example informative

--- examples/rules_engine.py
+++ examples/rules_engine.py
@@ -32,7 +32,7 @@ def print_stock():
     print(f"T-Shirts: {tshirt.inventory}")
     print(f"Jeans: {jeans.inventory}")
     print(f"Complete outfits we can sell: {outfit.inventory}")
-    print("--- stock take complete ---")
+    print("--- stock take complete ---\n")
 
 print("To begin with, here's our stock levels.")
 print_stock()
@@ -43,71 +43,83 @@ print_stock()
 # * if we have 0 tshirts in stock, order 1 tshirt
 
 def inventory_changed(me, affected_nodes, node_that_changed, attr_changed, cur_val, prev_val):
-    if prev_val is not None and cur_val > prev_val:
-        print('YOU ORDERED MORE OF OUTFIT!!!', node_that_changed)
+    # We will set this function to run whenever the rule (outfit) changes. This
+    # change can be triggered either by updating a property on the rule directly
+    # (i.e. `kb.rule('outfit(X, Y)').inventory = 1`) or when one of the nodes
+    # that feed the rule changes (i.e. `kb.node('tshirt').inventory = 1`)
+
+    if prev_val is not None and cur_val > prev_val and me == node_that_changed:
+        
+        # More outfits were ordered: behind the scenes, it means we ordered
+        # another t-shirt and another jeans.
+
         for node in affected_nodes:
-            print('ordering more of!', node)
-            node.inventory += 1
+            node.inventory += cur_val - prev_val
+
+    # Update our own inventory
+
     me.inventory = min([n.inventory for n in affected_nodes])
+
     if me.inventory is not None and me.inventory < 1:
-        print('ordering a single more of', node_that_changed, node_that_changed.inventory)
+        
+        # We sold something and are now out of stock.
+        # Better order more.
+
+        print('<<< Automatically ordering another jeans because we had 0 stock\n')
         node_that_changed.inventory += 1
-        print('after we ordered 1 more of :', node_that_changed, node_that_changed.inventory)
-        print('we got this many outfits', kb.rule(rule_num).inventory)
+
     else:
-        print('no need to order more!')
 
+        # We still have sufficient stock. 
+        pass
+
+
+# Set the function to run when the rule's computed value changes
 kb.rule(rule_num).on_change = inventory_changed
+
+# Right! Let's sell a jeans.
+# Notice how the number of sellable outfits goes down accordingly.
+
+print('>>> Selling one jeans\n')
 kb.node('jeans').inventory -= 1
-print('our inventory of outfits is', kb.rule(rule_num).inventory)
+
+print_stock()
+
+# And another
+
+print('>>> Selling one jeans\n')
 kb.node('jeans').inventory -= 1
-print('our inventory of outfits is', kb.rule(rule_num).inventory)
+
+print_stock()
+
+# And finally, let's sell another jeans, everyone loves jeans.
+# This one puts our inventory of jeans to 0; the rule will trigger
+# inventory_changed which will automatically order more.
+
+print('>>> Selling one jeans')
 kb.node('jeans').inventory -= 1
-print('our inventory of outfits is', kb.rule(rule_num).inventory)
+
+print_stock()
+
+# We only have 1 outfit to sell (luckily Zincbase ordered more.)
+# Let's order another outfit, as in, a set of tshirt and jeans.
+
+print('<<< Manually ordering another outfit set\n')
 kb.rule(rule_num).inventory += 1
-print('this num should be 1 bigger than the last:', kb.rule(rule_num).inventory)
-print('we got this jeans', kb.node('jeans').inventory)
-print('we gonna sell 1 jeans')
+
+print_stock()
+
+print('>>> So we have 2 jeans/11 tshirts left to sell (or 2 outfits).')
+print('>>> If we sell 1 jeans, we should have only 1 outfit left to sell.')
+print('>>> Selling 1 jeans\n')
+
+# Sell 1 more jeans
 kb.node('jeans').inventory -= 1
-print('now we got this many jeans', kb.node('jeans').inventory)
-print('after selling that, we got this many outfits', kb.rule(rule_num).inventory)
 
-import sys; sys.exit(0)
+print('After all these transactions, there should be only a single jeans in stock, meaning we can only sell 1 outfit too.\n')
 
-def low_stock_sku(sku, prev_val):
-    if sku.inventory > prev_val:
-        return
-    if 1 < sku.inventory < 5:
-        print(f'Order more of {sku}!')
-    elif sku.inventory < 1:
-        print(f'Out of stock of {sku}!')
+print_stock()
 
-def sold_an_outfit(edge, prev_val):
-    if prev_val < 1:
-        raise Exception('unable to sell')
-    for node in edge.nodes:
-        node.inventory -= 1
-    if edge.inventory < 1:
-        out_of_stock_of = []
-        for node in edge.nodes:
-            if node.inventory < 1:
-                out_of_stock_of.append(node)
-        print(f'Unable to sell any more of {edge} because we ran out of {out_of_stock_of}!')
-        for no_stock in out_of_stock_of:
-            print(f'Will restock {no_stock}')
-            kb.node(no_stock).restock(no_stock, 10)
-            
-def restock(sku, amount=1):
-    node = kb.node(sku)
-    sku.inventory += amount
-
-kb.node('tshirt').watch('inventory', low_stock_sku)
-kb.node('jeans').watch('inventory', low_stock_sku)
-kb.node('tshirt').restock = restock
-kb.node('jeans').restock = restock
-
-kb.node('tshirt').inventory -= 6
-
-kb.edge('tshirt', 'outfit', 'jeans').watch('inventory', sold_an_outfit)
-
-kb.edge('tshirt', 'outfit', 'jeans').inventory -= 1
+# The same results could totally be obtained using watch functions on individual
+# nodes. This was just an example on how to use rules to get there conceptually
+# more easily (depending on your thinking mode...)
